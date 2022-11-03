7778a0d6d2679c3d2313a91838fb28cc28b64b53
nicknterm <nikolasderm@gmail.com>
2022-11-03 09:24:12 +0200

    every feature is here. We are the feature

--- docReader/docReader.py
+++ docReader/docReader.py
@@ -134,7 +134,7 @@ def read_table(table):
 
 
 read_meals()
-document = Document('oktovrios-2022.docx')
+document = Document('programma-sitisis-noemvrioy-2022.docx')
 for table in document.tables:
     read_table(table)
 
