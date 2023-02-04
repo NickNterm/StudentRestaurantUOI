096f9554afd1a4e5a61670d5d5dac736cbd77f71
nicknterm <nikolasderm@gmail.com>
2023-02-04 11:50:19 +0200

    fixing the api with the special days

--- fastApi/main.py
+++ fastApi/main.py
@@ -81,14 +81,13 @@ def get_program(
 
 @app.get("/special")
 def get_special():
-    return {
-        [
-            {
-                "name": "Valentines Day",
-                "date": "14/02/2021",
-                "opacity": 0.5,
-                "background_image": "https://images.unsplash.com/photo-1604147706283-d7119b5b822c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d2hpdGV8ZW58MHx8MHx8&w=1000&q=80",
-            },
+    return
+    [
+        {
+            "name": "Valentines Day",
+            "date": "14/02/2021",
+            "opacity": 0.5,
+            "background_image": "https://images.unsplash.com/photo-1604147706283-d7119b5b822c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8d2hpdGV8ZW58MHx8MHx8&w=1000&q=80",
+        },
 
-        ]
-    }
+    ]
